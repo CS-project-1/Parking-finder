376fc6425e9a015bc5f7a46c0f5e3ca3bd2a1176
AndyOmondi17 <andyomondi17@gmail.com>
2021-07-14 12:50:25 +0300

    set up layout for the signUP and login page

---
+++ app/src/main/java/com/andy/smartparking/SignUp.java
@@ -0,0 +1,13 @@
+package com.andy.smartparking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
