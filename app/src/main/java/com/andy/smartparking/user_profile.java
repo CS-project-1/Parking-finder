f1176791e8c76cbac351f7304398b24a2b28e8fc
AndyOmondi17 <andyomondi17@gmail.com>
2021-07-16 12:13:53 +0300

    add user profile

--- app/src/main/java/com/andy/smartparking/user_profile.java
+++ app/src/main/java/com/andy/smartparking/user_profile.java
@@ -3,19 +3,63 @@ package com.andy.smartparking;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class user_profile extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference();
+        userID = user.getUid();
+
+        final TextView firstNameTextView = findViewById(R.id.firstnamep);
+        final TextView surnameTextView = findViewById(R.id.surnamep);
+        final TextView emailTextView = findViewById(R.id.email_addressp);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if(userProfile != null){
+                    String firstname = userProfile.firstName;
+                    String surname = userProfile.surName;
+                    String email = userProfile.email;
+
+                    firstNameTextView.setText(firstname);
+                    surnameTextView.setText(surname);
+                    emailTextView.setText(email);
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError error) {
+                Toast.makeText(user_profile.this,"An error occured",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void logout(View view) {
