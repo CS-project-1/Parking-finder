9fe2fcf7d7324fc457c61fb62f0477fb006f9ffa
AndyOmondi17 <andyomondi17@gmail.com>
2021-07-16 11:45:11 +0300

    add logout functionality

--- app/src/main/java/com/andy/smartparking/user_profile.java
+++ app/src/main/java/com/andy/smartparking/user_profile.java
@@ -1,14 +1,25 @@
 package com.andy.smartparking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class user_profile extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(user_profile.this,Login.class));
     }
 }
