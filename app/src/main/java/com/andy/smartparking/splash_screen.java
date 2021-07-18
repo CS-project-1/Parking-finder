facdb438bca26a16533ce4955334de1988e14653
AndyOmondi17 <andyomondi17@gmail.com>
2021-07-18 09:01:33 +0300

    add splash screen

--- app/src/main/java/com/andy/smartparking/splash_screen.java
+++ app/src/main/java/com/andy/smartparking/splash_screen.java
@@ -1,14 +1,28 @@
 package com.andy.smartparking;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splash_screen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(),Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
